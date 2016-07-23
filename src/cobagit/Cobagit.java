5fe4871a50bc406d92dc50520af31f8e35a474a5
Blepp <dixon.verrix13@gmail.com>
2016-07-23 16:11:19 +0700

    First Initialization !one1

--- src/cobagit/Cobagit.java
+++ src/cobagit/Cobagit.java
@@ -1,21 +1,27 @@
 package cobagit;
 
 import java.awt.EventQueue;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Cobagit extends JFrame {
+JLabel asdf = new JLabel();
 
     private static final long serialVersionUID = 1L;
-
+    
     public Cobagit() {
+        
         initUI();
     }
 
     private void initUI() {
-        setTitle("Simple example");
-        setSize(800, 600);
+        setTitle("Lul");
+        setSize(300, 600);
+        setUndecorated(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        asdf.setText("Hi guys");        
+        getContentPane().add(asdf);
+        asdf.setBounds(10, 30, 70, 14);
     }
 
     public static void main(String[] args) {
